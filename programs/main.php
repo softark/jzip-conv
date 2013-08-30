0ea4fec724922cab81ab93f5681349bdeedc0c1f
softark <softark@gmail.com>
2013-08-31 08:10:16 +0900

    1308 update

--- programs/main.php
+++ programs/main.php
@@ -46,7 +46,7 @@ require_once('ZipData.php');
 require_once('ZipBizData.php');
 
 /** @var $yearMonth string 年月 */
-$yearMonth = '1307';
+$yearMonth = '1308';
 
 $converter = new ZipDataConverter($yearMonth);
 $converter->runConversion();
