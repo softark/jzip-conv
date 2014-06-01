97d5c5b0351c94ac5cbc8f991ff7c2ba40dbe80e
Nobuo Kihara <kihara@softark.net>
2014-06-01 22:09:40 +0900

    2014-05 update

--- programs/main.php
+++ programs/main.php
@@ -46,7 +46,7 @@ require_once('ZipData.php');
 require_once('ZipBizData.php');
 
 /** @var $yearMonth string 年月 */
-$yearMonth = '1404';
+$yearMonth = '1405';
 
 $converter = new ZipDataConverter($yearMonth);
 $converter->runConversion();
