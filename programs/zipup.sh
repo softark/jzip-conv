025ae75dd89a6dadd5b2c004645e957596b78ddb
Nobuo Kihara <softark@gmail.com>
2020-10-05 10:28:40 +0900

    shell command bug fixed

--- programs/zipup.sh
+++ programs/zipup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 php ./zipconv.php
-YM=$(date -d '1 months ago' '+%ym')
+YM=$(date -d '1 months ago' '+%y%m')
 mysql --user=root --password=isa563rigami zipdata < ../outputs/updates/update_${YM}.sql
